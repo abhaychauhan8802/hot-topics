1e8ed5d1b689c1b0894bc577ddaaa685fa5835db
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2025-09-29 14:28:29 +0530

    fix progress bar delay

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
 export default function Footer() {
@@ -17,18 +18,18 @@ export default function Footer() {
 
           {/* Links */}
           <nav className="flex flex-wrap justify-center mt-4 space-x-4 md:mt-0">
-            <a href="/about" className="hover:underline">
+            <Link href="/about" className="hover:underline">
               About
-            </a>
-            <a href="/disclaimer" className="hover:underline">
+            </Link>
+            <Link href="/disclaimer" className="hover:underline">
               Disclaimer
-            </a>
-            <a href="/privacy-policy" className="hover:underline">
+            </Link>
+            <Link href="/privacy-policy" className="hover:underline">
               Privacy Policy
-            </a>
-            <a href="/contact" className="hover:underline">
+            </Link>
+            <Link href="/contact" className="hover:underline">
               Contact
-            </a>
+            </Link>
           </nav>
         </div>
 
@@ -37,38 +38,38 @@ export default function Footer() {
 
         {/* Social Links */}
         <div className="flex justify-center space-x-6">
-          <a
+          <Link
             href="https://www.facebook.com/share/19TihMhz6n/"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-blue-500"
           >
             <FaFacebook size={20} />
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://www.instagram.com/govtdeskhub?igsh=MTY0b3F6aTFhM2Ewag=="
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-pink-500"
           >
             <FaInstagram size={20} />
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://x.com/govtdeskhub?t=d5gvC4vb72gVO_skMyRnEw&s=09"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-sky-400"
           >
             <FaTwitter size={20} />
-          </a>
-          <a
+          </Link>
+          <Link
             href="https://youtube.com/@govtdesk_hub?si=VYKrb7eh120FTKz1"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-red-500"
           >
             <FaYoutube size={20} />
-          </a>
+          </Link>
         </div>
       </div>
     </footer>
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,24 +1,67 @@
+// "use client";
+
+// import "../styles/progressbar.css";
+// import { useEffect } from "react";
+// import { usePathname } from "next/navigation";
+// import NProgress from "nprogress";
+// import "nprogress/nprogress.css";
+
+// NProgress.configure({ showSpinner: false, easing: "ease", speed: 500 });
+
+// export default function ProgressBar() {
+//   const pathname = usePathname();
+
+//   useEffect(() => {
+//     NProgress.set(0.0);
+//     NProgress.set(0.2);
+//     NProgress.set(0.4);
+//     NProgress.set(0.6);
+//     NProgress.set(0.8);
+//     NProgress.set(1);
+//   }, [pathname]);
+
+//   return null;
+// }
+
 "use client";
 
-import "../styles/progressbar.css";
 import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 import NProgress from "nprogress";
-import "nprogress/nprogress.css";
+import "../styles/progressbar.css";
 
-NProgress.configure({ showSpinner: false, easing: "ease", speed: 500 });
+NProgress.configure({ showSpinner: false, easing: "ease", speed: 800 });
 
 export default function ProgressBar() {
   const pathname = usePathname();
 
   useEffect(() => {
-    NProgress.set(0.0);
-    NProgress.set(0.2);
-    NProgress.set(0.4);
-    NProgress.set(0.6);
-    NProgress.set(0.8);
-    NProgress.set(1);
+    // stop the bar when route changes
+    NProgress.done();
   }, [pathname]);
+
+  useEffect(() => {
+    // start bar immediately on link click
+    const handleClick = (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+      const anchor = target.closest("a") as HTMLAnchorElement | null;
+
+      if (
+        anchor &&
+        anchor.href.startsWith(window.location.origin) && // internal link
+        !anchor.target && // not opening in new tab
+        !e.metaKey &&
+        !e.ctrlKey &&
+        !e.shiftKey &&
+        !e.altKey // no modifier keys
+      ) {
+        NProgress.start();
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, []);
 
   return null;
 }
